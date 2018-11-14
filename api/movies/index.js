81f248c68df106999a0a299b58ea4a00cdeb5fc3
Kevin Cabrera <keviincabrera@gmail.com>
2018-11-14 22:12:11 +0000

    Añadido controlador postMovie

--- api/movies/index.js
+++ api/movies/index.js
@@ -5,17 +5,7 @@ const movies = require('./../../data/movies.json');
 const movieController = require('./../../controllers/movies/index.js');
 
 router.get('/', (require, resolve) => movieController.getMovies(require, resolve));
-
-router.post('/', (require, resolve) => {
-    const newMovie = require.body;
-    const movieAlredyExists = movies.find( movie => movie.name == newMovie.name);
-    if(movieAlredyExists){
-        resolve.send(`Film < ${newMovie.name} > alredy exists in the database`)
-    } else {
-        movies.push(newMovie);
-        resolve.send(`Film < ${newMovie.name} > saved correctly!`);
-    }
-});
+router.post('/', (require, resolve) => movieController.postMovie(require, resolve));
 
 module.exports = router;
 
--- controllers/movies/index.js
+++ controllers/movies/index.js
@@ -4,6 +4,18 @@ const getMovies = (require, resolve) => {
     resolve.send(movies);
 }
 
+const postMovie = (require, resolve) => {
+    const newMovie = require.body;
+    const movieAlredyExists = movies.find( movie => movie.name == newMovie.name);
+    if(movieAlredyExists){
+        resolve.send(`Film < ${newMovie.name} > alredy exists in the database`)
+    } else {
+        movies.push(newMovie);
+        resolve.send(`Film < ${newMovie.name} > saved correctly!`);
+    }
+}
+
 module.exports = {
-    getMovies
+    getMovies,
+    postMovie
 }
