429b31e4d55af380da63792df2b1f0bee8088b19
Kevin Cabrera <keviincabrera@gmail.com>
2018-11-14 23:41:36 +0000

    ~

--- api/movies/index.js
+++ api/movies/index.js
@@ -6,7 +6,7 @@ const movieController = require('./../../controllers/movies/index.js');
 router.get('/', (request, resolve) => movieController.getMovies(request, resolve));
 router.post('/', (request, resolve) => movieController.postMovie(request, resolve));
 router.post('/like/:id', (request, resolve) => movieController.likeMovie(request, resolve));
-router.post('/dislike/:id', (request, resolve) => movieController.dislikeMovie(request, resolve));
+router.delete('/dislike/:id', (request, resolve) => movieController.dislikeMovie(request, resolve));
 
 module.exports = router;
 
