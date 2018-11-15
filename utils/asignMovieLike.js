7426b6f946fbf6cb586b14e8f728109e0af74998
Kevin Cabrera <keviincabrera@gmail.com>
2018-11-15 00:09:01 +0000

    Añadida funcionalidad de guardado con movieLike y movieDislike

--- utils/asignMovieLike.js
+++ utils/asignMovieLike.js
@@ -1,13 +1,16 @@
 const movies = require('./../data/movies.json');
+const files = require('./files.js');
+
 
 const asignMovieLike = (request, resolve, bool) => {
     const movieId = request.params.id;
     const movie = movies.find( movie => movie.id == movieId);
     if (movie) {
         movie.like = bool;
+        files.saveFile(movies);
+        resolve.send(`Movie with id ${movieId} has got a like!!`);
     }
     else resolve.send(`Movie with id ${movieId} do not exists in database`);
-
 }
 
 module.exports = asignMovieLike;
